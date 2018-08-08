5d975cf19260c51298093f540f0d46aa51467f89
hosodahiroshi <applef_loveg_mea@icloud.com>
2018-08-08 22:09:56 +0900

    setup firebase auth

--- Rx_TubeApp/Application/AppDelegate.swift
+++ Rx_TubeApp/Application/AppDelegate.swift
@@ -9,21 +9,25 @@
 import UIKit
 import RxSwift
 import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+    
     var window: UIWindow?
     private var appCoordinator: AppCoordinator!
     private let disposeBag = DisposeBag()
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow()
         
-        FirebaseApp.app()
+        // Config Firebase
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance().delegate = self
         
+        // Config Coordinator
         appCoordinator = AppCoordinator(window: window!)
         appCoordinator.start()
             .subscribe()
@@ -31,6 +35,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
-
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url,
+                                                 sourceApplication: options[.sourceApplication] as? String,
+                                                 annotation: [:])
+    }
+    
+    func sign(_ signIn: GIDSignIn?, didSignInFor user: GIDGoogleUser?, withError error: Error?) {
+        
+        if let _ = error {
+            // TODO: Implement Error Handling
+            return
+        }
+        
+        guard let authentication = user?.authentication else { return }
+        let credential = GoogleAuthProvider.credential(
+            withIDToken: authentication.idToken,
+            accessToken: authentication.accessToken)
+        
+        Auth.auth().signInAndRetrieveData(with: credential) { (result, error) in
+            if let _ = error {
+                // TODO: Implement Error Handling
+                return
+            }
+            
+            
+        }
+    }
 }
-
